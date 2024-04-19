1dac102062a46b100d984fc591485a310c890ca1
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2024-04-19 22:15:25 +0200

    Initial commit
    Add linux fontconfig implementation

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+
+#include "shl/print.hpp"
+#include "find_font.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        put("Error: expecting name and style arguments\n");
+        return 1;
+    }
+
+    ff_cache *c = ff_load_font_cache();
+
+    if (c == nullptr)
+    {
+        put("Error: could not load font cache\n");
+        return 2;
+    }
+
+    const char **font_names_and_styles = (const char**)argv + 1;
+    int count = argc - 1; 
+    int found_index = -1;
+    const char *path = ff_find_first_font_path_vague(c, font_names_and_styles, count, &found_index);
+    int ret_code = 0;
+
+    if (path == nullptr)
+    {
+        tprint("No path found.\n");
+        ret_code = 3;
+    }
+    else
+        tprint("Path for font '%': %\n", font_names_and_styles[found_index], path);
+
+    ff_unload_font_cache(c);
+
+    return ret_code;
+}
